03dc00b8beb9d42b21b8b2babde4a1aebb3119ee
Katoon <katoonlion@gmail.com>
2025-11-03 01:21:25 -0500

    update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 
 /*---------- Variables (state) ---------*/
 
-// start game
+// Define constants and variables
+
 let started = false;
 let level = 0;
 
@@ -16,30 +17,34 @@ let playerPatternColor = [];
 
 /*----- Cached Element References  -----*/
 
+// Cache DOM element
+
 const pressStartElement = document.querySelector('#start-btn');
 const changeTitletoScore = document.querySelector('#title');
 const buttonColorElement = document.querySelectorAll('.btn');
 
 /*-------------- Functions -------------*/
 
-//if player press 'S' key ---> start game!
-
+// Function game start
 function gameStarted() {
-    // If player press 'S' ---> game started = true --> title text change to score// button start game --> hide
+    // If game has not start
+    // player press start --> game started = true
+
     // !started --> true
     if(!started){
-        // button start --> hide
+        started = true;
+        // hide start game button
         pressStartElement.style.display = 'none';
-        // title text change to level score
+        // reset title tex to level score 
         changeTitletoScore.textContent = `level ${level}`;
-        // game start random
+        // call function start random color
         startRandom();
     };
 };
 
-
-// Start random color
+// Function start random color
 function startRandom() {
+    // random color from option → store to game pattern array
     // random index of color <= 4
     let randomIndexColor = Math.floor(Math.random()*4);
     // If index = 0 --> randomColor = 'green'
@@ -47,67 +52,87 @@ function startRandom() {
     // put it in array gameRandomColor
     gameRandomColor.push(randomColor);
 
+    // 	increase level
+    level ++;
+
+    // 	update title and and current level
+    changeTitletoScore.textContent = `level ${level}`;
+    // clear player pattern for next round
+    playerPatternColor = [];
+
+    // play sound and flash on color chosen
     // sound at color of game random
     playSound(randomColor);  
 
     // fade color at color of game random
-    fadeColor(randomColor);
+    flashColor(randomColor);
 
-    // Cleared array before player play new game
-    playerPatternColor = [];
-
-    level ++;
-    changeTitletoScore.textContent = `level ${level}`;
 };
 
-// function play sound
+// Function player click
+function handlePlayerClick(event) {
+    // add color from player chosen to player patter array
+    // If player click on red store red to the platerPattern color array
+    let playerClickOnColor = event.target.id;
+    playerPatternColor.push(playerClickOnColor);
+
+    // 	play sound and flash on color chosen
+    playSound(playerClickOnColor);
+
+    flashColor(playerClickOnColor);
+
+    // call to function check player answer for check last index of player was click
+    checkPlayerAnswer(playerPatternColor.length-1);
+}
+
+// Function play sound when player click on each color
 function playSound(name) {
     // let audio = new Audio("sounds/" + color + ".mp3");
     let audio = new Audio(`sounds/${name}.mp3`);
     audio.play();
 }
 
-// function animation
-function fadeColor(selectedColor) {
+// Function flash color when player click on each color
+function flashColor(selectedColor) {
     document.querySelector(`#${selectedColor}`).classList.add("pressedColor");
     setTimeout(() => {
          document.querySelector(`#${selectedColor}`).classList.remove("pressedColor");
     }, 300);
 };
 
-// Check player answer
-
+// Function check player answer
 function checkPlayerAnswer(currentLevel) {
-    // If player click on correct answer --> correct index of random
+    // If player click on correct answer --> correct index of game random
     if(playerPatternColor[currentLevel] === gameRandomColor[currentLevel]) {
-        // If player repeat all of random color respectively
+        // If player pattern = game pattern length
         if(playerPatternColor.length === gameRandomColor.length) {
-        // set time for start new sequence
+        // wait and call function start random color for start random new round
             setTimeout(() => {
                startRandom();   
             }, 800);
                       
         }
 
-    } else { // If wrong  
-        // sound wrong color
-        playSound('wrong');        
-        // text "Game Over! Click for restart "
-        changeTitletoScore.textContent = 'Game Over! Click for restart';
-        // show button start
-        pressStartElement.style.display = '';
-
+    } else {
+        // play wrong sound and flash red color on display screen
+        playSound('wrong');               
         // display show red flash 
         document.querySelector('body').classList.add('game-Over');
         setTimeout(() => {
             document.querySelector('body').classList.remove('game-Over');
         }, 500);
-        // call function gameOver for go back to begin
-        gameOver();
+
+        // update title to ‘Game Over! Press for Restart’
+        changeTitletoScore.textContent = 'Game Over! Press for restart';
+        // show button start game
+        pressStartElement.style.display = '';        
+        // call to function game over for set to begin
+        gameOver();  
     }
       
 }
 
+// Function game over
 function gameOver() {
     // set to begin
     level = 0;
@@ -116,6 +141,7 @@ function gameOver() {
 }
 
 /*----------- Event Listeners ----------*/
+// Add event listeners
 
 // Player click for start game
 pressStartElement.addEventListener('click', gameStarted);
@@ -124,51 +150,64 @@ pressStartElement.addEventListener('click', gameStarted);
 // Select all button
 buttonColorElement.forEach(function(button) {
     // Add event listener each button color
-    button.addEventListener('click', function (event) {
-        // If player click on red store red to the platerPattern color array
-        let playerClickOnColor = event.target.id;
-        playerPatternColor.push(playerClickOnColor);
-        playSound(playerClickOnColor);
+    button.addEventListener('click', handlePlayerClick) ;
 
-        fadeColor(playerClickOnColor);
+});
 
-        // Check last index of color of player was click and call to function checkPlayerAnswer()
-        checkPlayerAnswer(playerPatternColor.length-1);
-    });
 
-})
-// START the game
+/* 
+// // Define constants and variables
+// Define all colors option = ['green', 'yellow', 'red', 'blue']
+// Declare started = false
+// Declare level = 0
+// Declare game pattern = [ ] 
+// Declare player pattern = [ ]  
 
-// CREATE an empty list called gamePattern
-// CREATE an empty list called userPattern
-// SET level to 0
+// // Cache DOM element
+// Set start game button element = #start-btn
+// Set title element = #title
+// Set button color element = .btn // NodeList of 4
 
-// FUNCTION startGame
-    // CALL nextSequence
+// // Add event listeners
+// ADD event listener to start game button → function game start
+// ADD event listener to all button color → function player click on button
 
-// FUNCTION nextSequence
-    // INCREASE level by 1
-    // CLEAR userPattern
-    // CHOOSE a random color from [red, blue, green, yellow]
-    // ADD that color to gamePattern
-    // SHOW the full sequence of colors to the player (flash + sound)
+// // Function game start
+// 	If game has not start
+// 		set started = true
+// hide start game button
+// reset title tex to level score 
+// call function start random color
 
-// WHEN the player clicks a color
-    // ADD that color to userPattern
-    // PLAY the sound for that color
-    // CALL checkAnswer
+// // Function start random color
+// 	random color from option → store to game pattern array
+// 	increase level
+// 	update title and and current level
+// 	clear player pattern for next round
+// 	play sound and flash on color chosen
 
-// FUNCTION checkAnswer
-    // IF the most recent color in userPattern matches gamePattern
-        // IF userPattern is the same length as gamePattern
-            // WAIT one second
-            // CALL nextSequence
-    // ELSE
-        // PLAY “wrong” sound
-        // SHOW “Game Over” message
-        // CALL resetGame
+// // Function player click
+// 	add color from player chosen to player patter array
+// 	play sound and flash on color chosen
+// 	call to function check player answer for check last index of player was click
 
-// FUNCTION resetGame
-    // SET level to 0
-    // CLEAR gamePattern
-    // WAIT for player to press a key to start again
+// // Function play sound when player click on each color
+// // Function flash color when player click on each color
+
+// // Function check player answer
+// 	If player click on correct answer --> correct index of game random
+// 		If player pattern = game pattern length
+// 			wait and call function start random color for start random new round
+// 	else
+// 		play wrong sound and flash red color on display screen
+// 		update title to ‘Game Over! Press for Restart’
+// 		show button start game
+// 		call to function game over for set to begin
+
+
+// // Function game over
+// 	set level = 0
+// 	game pattern = [ ]
+// 	started = false
+
+*/
